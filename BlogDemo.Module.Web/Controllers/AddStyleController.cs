1ffd28de51bb39d2d94df387ade298ab963785bb
Jose Javier Columbie <jose@shopcontroller.com>
2020-04-01 23:51:40 -0700

    3

---
+++ BlogDemo.Module.Web/Controllers/AddStyleController.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.ExpressApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace BlogDemo.Module.Web.Controllers
+{
+    public class AddStyleController : ViewController
+    {
+
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+
+            LiteralControl style = new LiteralControl();
+            style.Text = "<style type='text/css' scoped>.white { background-color: palegreen; }</style>";
+            ((Control)View.Control).Controls.Add(style);
+
+
+        }
+    }
+}
